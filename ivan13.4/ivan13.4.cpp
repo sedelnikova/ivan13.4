fa6d2d984ac81bd5df0ff8721343ad74f1c06a51
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-10-25 18:39:58 +0300

    Добавьте файлы проекта.

---
+++ ivan13.4/ivan13.4.cpp
@@ -0,0 +1,31 @@
+﻿// Дан массив A размера N. Вывести его элементы в следующем порядке: A1, AN , A2, AN−1, A3, AN−2.....
+
+#include <iostream>
+#include <stdio.h> 
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int N, i, f;
+	cout << "Введите, пожалуйста, кол-во элементов массива (N > 2): ";
+	cin >> N;
+	int* arr = new int[N];
+
+	cout << "Введите, пожалуйста, массив: ";
+	for (i = 1; i <= N; i++)
+	{
+		cin >> arr[i];
+	}
+	f = N / 2;
+	for (i = 1; i <= f; i++)
+	{
+		cout << arr[i] << " " << arr[N - i + 1] << " ";
+
+	}
+	if (N % 2 != 0)
+	{
+		cout << arr[f + 1] << " ";
+	}
+	return 0;
+}
